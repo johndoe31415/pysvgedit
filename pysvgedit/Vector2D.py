acdd018c6feabdb4786a1f71e223e0b1da023f5a
Johannes Bauer <joe@johannes-bauer.com>
2023-06-22 20:01:53 +0200

    Added some helpful 2D computation functions
    This is going to improve vector handling for users.

--- pysvgedit/Vector2D.py
+++ pysvgedit/Vector2D.py
@@ -18,6 +18,8 @@
 #	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
+import math
+
 class Vector2D():
 	def __init__(self, x = 0, y = 0):
 		self._x = x
@@ -31,8 +33,33 @@ class Vector2D():
 	def y(self):
 		return self._y
 
+	@property
+	def length(self):
+		return math.sqrt((self.x ** 2) + (self.y ** 2))
+
+	@property
+	def norm(self):
+		return self / self.length
+
+	@property
+	def ortho(self):
+		return Vector2D(self.y, self.x)
+
+	def __matmul__(self, other):
+		# Scalar product
+		return (self.x * other.y) - (other.x * self.y)
+
 	def __add__(self, vector):
 		return Vector2D(self.x + vector.x, self.y + vector.y)
+
+	def __mul__(self, scalar):
+		return Vector2D(self.x * scalar, self.y * scalar)
+
+	def __rmul__(self, scalar):
+		return self * scalar
+
+	def __truediv__(self, divisor):
+		return Vector2D(self.x / divisor, self.y / divisor)
 
 	def __repr__(self):
 		return f"<{self.x:.1f}, {self.y:.1f}>"
